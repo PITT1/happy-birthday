996387b7fdb9ab97cb1fa0458c128c92ec582cc3
PITT1 <petroleo.plta.97@gmail.com>
2024-04-18 18:26:02 -0400

    corrigiendo estetica

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import HappyBirthday from './components/HappyBirthday';
-import { motion } from 'framer-motion';
-import conffeti from 'canvas-confetti';
-import './App.css';
+import { useState } from "react";
+import HappyBirthday from "./components/HappyBirthday";
+import { motion } from "framer-motion";
+import conffeti from "canvas-confetti";
+import "./App.css";
 
 function App() {
   const [clicked, setClicked] = useState(false);
@@ -10,14 +10,18 @@ function App() {
   const hadleClick = () => {
     setClicked(true);
     conffeti();
-  }
+  };
 
   return (
-    <motion.main initial={{opacity: 0}} animate={{opacity: 1}}>
-    {!clicked && <button  onClick={hadleClick} className='button1'>cliquez sur moi :3</button>}
-    { clicked && <HappyBirthday/>}
+    <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      {!clicked && (
+        <button onClick={hadleClick} className="button1">
+          cliquez sur moi :3
+        </button>
+      )}
+      {clicked && <HappyBirthday />}
     </motion.main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/HappyBirthday.jsx
+++ src/components/HappyBirthday.jsx
@@ -1,13 +1,33 @@
 import "../App.css";
 import chems from "../../public/chems1.jpg";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const HappyBirthday = () => {
   return (
-    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} className="birthday">
-      <motion.h1 transition={{delay: 0.2 }} initial={{opacity: 0}} animate={{opacity: 1}}>joyeux anniversaire Darin!! :D</motion.h1>
-      <motion.img transition={{delay: 0.6, duration: 1}} initial={{scale: 0, rotate: 360}} animate={{scale: 1, rotate: 0}} src={chems} alt="el perrito chems de cumpleaños :D" />
-      <motion.h2 transition={{delay: 1.5}} initial={{opacity: 0}} animate={{opacity: 1}}>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="birthday"
+    >
+      <motion.h1
+        transition={{ delay: 0.2 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      >
+        joyeux anniversaire Darin!! :D
+      </motion.h1>
+      <motion.img
+        transition={{ delay: 0.6, duration: 1 }}
+        initial={{ scale: 0, rotate: 360 }}
+        animate={{ scale: 1, rotate: 0 }}
+        src={chems}
+        alt="el perrito chems de cumpleaños :D"
+      />
+      <motion.h2
+        transition={{ delay: 1.5 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      >
         Celui qui vous a dit que vous vieillissiez est très confus. Tu te
         bonifies comme le vin, tu es millésimé !
       </motion.h2>
