8726b2e00af136875e77f673fe488e0afdba9ad0
PITT1 <petroleo.plta.97@gmail.com>
2024-04-18 18:23:08 -0400

    se agrego el conffeti

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react';
 import HappyBirthday from './components/HappyBirthday';
 import { motion } from 'framer-motion';
+import conffeti from 'canvas-confetti';
 import './App.css';
 
 function App() {
   const [clicked, setClicked] = useState(false);
 
+  const hadleClick = () => {
+    setClicked(true);
+    conffeti();
+  }
 
   return (
     <motion.main initial={{opacity: 0}} animate={{opacity: 1}}>
-    {!clicked && <button  onClick={() => {setClicked(true)}} className='button1'>cliquez sur moi :3</button>}
+    {!clicked && <button  onClick={hadleClick} className='button1'>cliquez sur moi :3</button>}
     { clicked && <HappyBirthday/>}
     </motion.main>
   )
